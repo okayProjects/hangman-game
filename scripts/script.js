969d54f41bc5e6e42a046ab873e588bc5cc64093
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-10-20 20:30:07 +0200

    Add files

--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,50 @@
-const cardsColor = ['red', 'red', 'green', 'green', 'blue', 'blue', 'yellow', 'yellow', 'orange', 'orange', 'purple', 'purple', 'greenyellow', 'greenyellow', 'lightsalmon', 'lightsalmon', 'brown', 'brown'];
+const alphabet = ["A", "Ą", "B", "C", "Ć", "D", "E", "Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N", "Ń", "O", "Ó", "P", "Q", "R", "S", "Ś", "T", "U", "V", "W", "X", "Y", "Z", "Ż", "Ź"];
 
-let cards = document.querySelectorAll('.box-wrapper>div');
-cards = [...cards];
-const startTime = new Date().getTime();
-let activeCard = '';
-const activeCards = [];
-const gamePairs = cards.length / 2;
-let gameResult = 0;
+const proverbs = ['bez pracy nie ma kołaczy', 'kto rano wstaje temu pan bóg daje', 'gdyby jaś się nie nauczył to by jan nie umiał', 'gdzie kucharek sześć tam cycków dwanaście', 'kuj żelazo póki gorące'];
+
+let slogan = '';
+const button = document.querySelector('button');
+let divs = [];
 
 
-const clickCard = function () {
-    activeCard = this;
-    if (activeCard == activeCards[0]) return;
-    activeCard.classList.remove('hidden');
-    if (activeCards.length === 0) {
-        activeCards[0] = activeCard;
-        return
-    } else {
-        cards.forEach(card => card.removeEventListener('click', clickCard));
-        activeCards[1] = activeCard;
-        setTimeout(() => {
-            if (activeCards[0].className === activeCards[1].className) {
-                activeCards.forEach(card => card.classList.add('off'));
-                gameResult++;
-                cards.filter((card) => !card.classList.contains('off'));
-                if (gameResult == gamePairs) {
-                    const endTime = new Date().getTime();
-                    alert(`Gratulacje, udaøo sié!!! Gra zakoñczona w ${(startTime - endTime)/1000} sekund`);
-                    location.reload;
-                }
-            } else {
-                activeCards.forEach(card => card.classList.add('hidden'));
-            }
-            activeCard = '';
-            activeCards.length = 0;
-            cards.forEach(card => card.addEventListener('click', clickCard));
-        }, 700);
+const drawnSlogan = () => {
+    const index = Math.floor(Math.random() * proverbs.length);
+    slogan = proverbs[index].toUpperCase();
+    proverbs.splice(index, 1);
+}
+
+const displaySlogan = () => {
+    let dashedSlogan = '';
+    for (let i = 0; i < slogan.length; i++) {
+        if (slogan.charAt(i) == ' ') {
+            dashedSlogan += " ";
+        } else {
+            dashedSlogan += '-';
+        }
+    }
+    document.querySelector('.slogan p').innerHTML = dashedSlogan;
+}
+
+const letterWrapper = () => {
+    for (let i = 0; i < alphabet.length; i++) {
+        const div = document.createElement('div');
+        div.classList.add('letter-container');
+        div.innerHTML = alphabet[i];
+        document.querySelector('.alphabet').appendChild(div);
     }
 }
 
-const init = () => {
-    cards.forEach(card => {
-        const index = Math.floor(Math.random() * cardsColor.length);
-        card.classList.add(cardsColor[index]);
-        cardsColor.splice(index, 1);
-    });
-    setTimeout(() => {
-        cards.forEach(card => {
-            card.classList.add('hidden');
-            card.addEventListener('click', clickCard);
-        })
-    }, 1500);
+const game = function () {
+    console.log(this.innerHTML);
 }
 
-init();
+button.addEventListener('click', () => {
+    button.classList.add('off');
+    document.querySelector('.container').classList.remove('off');
+    drawnSlogan();
+    displaySlogan();
+    letterWrapper();
+    divs = document.querySelectorAll('.alphabet div');
+    divs = [...divs];
+    divs.forEach(div => div.addEventListener('click', game));
+});
